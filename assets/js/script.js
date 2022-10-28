6715c8db98af59e4eab760d4712fb90f04e9dbed
kirbyfridge <114848971+kirbyfridge@users.noreply.github.com>
2022-10-27 21:36:41 -0400

    made dataset number visible when clicked on

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,4 +4,15 @@ container.addEventListener("click", function(event) {
   var element = event.target;
 
   // TODO: Complete function
-});
+  if (element.matches(".box")) {
+    var state = element.getAttribute("data-state");
+
+  if (state === "hidden") {
+    element.textContent = element.dataset.number;
+    element.dataset.state = "visible";
+}
+  else {
+    element.textContent = " ";
+    element.setAttribute("data-state", "hidden")
+  }
+}});
